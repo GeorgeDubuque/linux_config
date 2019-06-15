34c0617e24a1dfdb9f95c77d91f3194e98bfdb31
George Dubuque <dubuqug@wwu.edu>
2019-06-14 22:07:33 -0700

    source rc files

--- setup.sh
+++ setup.sh
@@ -50,3 +50,5 @@ cp -f ./.bashrc ~/.bashrc
 cp -f ./.zshrc ~/.zshrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp -f ./.vimrc ~/.vimrc
+source ~/.bashrc
+source ~/.zshrc
