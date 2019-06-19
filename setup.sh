01b20a6be7df974bf16c925ebdfe7db779dea0fd
George Dubuque <dubuqug@wwu.edu>
2019-06-19 11:33:08 -0700

    set def shell to zsh running vundle

--- setup.sh
+++ setup.sh
@@ -27,12 +27,11 @@ prog="oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 #Setup shell
-cp -f ./.bashrc ~/.bashrc
 cp -f ./.zshrc ~/.zshrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp -f ./.vimrc ~/.vimrc
-source ~/.bashrc
-source ~/.zshrc
+vim +PluginInstall +qall
+usermod --shell /bin/zsh $USER
 
 #Load shell profile
 dconf load /org/gnome/terminal/legacy/profiles:/:1430663d-083b-4737-a7f5-8378cc8226d1/ < ./shell-theme.dconf
